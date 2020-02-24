5c8de3dc854ca83b96b0d3afa0641fc449a8957d
Armando <armando.josue.og@gmail.com>
2020-02-23 20:57:15 -0800

    open and closed lines as components

--- src/view/first_hexagram.mjs
+++ src/view/first_hexagram.mjs
@@ -1,16 +1,19 @@
 import { div } from "muvjs/muv-dom";
 
+export const OpenLine = props => children => "＿＿＿　＿＿＿";
+export const ClosedLine = props => children => "＿＿＿＿＿＿＿";
+
 export const FirstHexagram = dispatch => model => 
 div()(
   model.lines.map(lineValue => 
-    div()(lineValue % 2 === 0 ? "＿＿＿　＿＿＿" : "＿＿＿＿＿＿＿")
+    div()(lineValue % 2 === 0 ? OpenLine()() : ClosedLine()())
     )
 )
 
 export const SecondHexagram = dispatch => model => 
 div()(
   model.lines.map(lineValue => 
-    div()((lineValue % 2 === 0 && lineValue !== 6)
-    || (lineValue % 2 === 1 || lineValue !== 9) ? "＿＿＿　＿＿＿" : "＿＿＿＿＿＿＿")
+    div()((lineValue % 2 === 0 || lineValue === 9)
+    && (lineValue % 2 === 0 || lineValue === 6) ? OpenLine()() : ClosedLine()())
     )
 )
