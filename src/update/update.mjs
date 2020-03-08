7f1ffe5934cac8d79371afc44372e25ee182e03f
Armando <armando.josue.og@gmail.com>
2020-03-08 16:44:56 -0700

    divide sections

--- src/update/update.mjs
+++ src/update/update.mjs
@@ -1,3 +1,6 @@
+const HEADS_VALUE = 3;
+const TAILS_VALUE = 2;
+
 export const update = model => action => {
   let effects = [];
   switch (action.type) {
@@ -7,18 +10,20 @@ export const update = model => action => {
       model.coins[action.r] = model.coins[action.r].slice();
       model.coins[action.r][action.c] = action.value;
 
-      model.lines = model.lines.slice();
-      model.lines[action.r] = model.coins[action.r].reduce((a, b) => {
-        return a + b
-      }, 0);
+      if(model.coins[action.r].filter(side => !!side).length === 3){
+        model.lines = model.lines.slice();
+        model.lines[action.r] = model.coins[action.r].reduce((a, b) => {
+          return a + b
+        }, 0);
+      }
 
       break;
 
-      case "choose-coin-totals":
-        model = {...model};
-        model.lines = model.lines.slice();
-        model.lines[action.r] = action.value;
-        
+    case "choose-coin-totals":
+      model = {...model};
+      model.lines = model.lines.slice();
+      model.lines[action.r] = action.value;
+
       break;
 
   }
--- src/view/view.mjs
+++ src/view/view.mjs
@@ -1,9 +1,15 @@
-import { div, button } from 'muvjs/muv-dom';
+import { div, button, h1, h2, h3, h5, a, span } from 'muvjs/muv-dom';
 import { AllCoinOptions } from './coin_options.mjs'
 import { LineNumbers } from './line_numbers.mjs'
 import { FirstHexagram, SecondHexagram, ChangingLines } from './hexagrams.mjs'
+import { chooseCoinSides } from '../update/actions.mjs';
+import {CoinSelection} from './coin_options.mjs';
 
 export const view = dispatch => {
+
+  const dispatcher = {
+    chooseCoinSides: (r, c, value) => dispatch(chooseCoinSides(r, c, value))
+  };
 
   const AllCoinOptionsContent = AllCoinOptions(dispatch);
   const LineNumbersContent = LineNumbers(dispatch);
@@ -14,14 +20,46 @@ export const view = dispatch => {
   return model =>
     div()(
       [
-        , div({ class: 'flex flex-row'})([
-          , AllCoinOptionsContent(model)
-          , LineNumbersContent(model)
-        ]),
-        , div({ class: 'flex flex-row'})([
+        , div({class: 'w-100 ma3 tc tracked sans-serif'})([
+          , h1({class: 'f1'})('I-CHING')
+          , h2()('The Book of Changes')
+          , div({class: 'serif f5 o-70'})([
+            , div()('Iching only requires you to roll 3 coins, 6 times.')
+            , div()([
+              , span()('And have an ')
+              , a({href: '#'})('Iching book')
+              , span()(' ready.')
+            ])
+          ])
+        ])
+        , div({ class: 'flex flex-row ma3 w-100 justify-center'})([
           , FirstHexagramContent(model)
           , ChangingLinesContent(model)
           , SecondHexagramContent(model)
+        ])
+        , div()([
+          , div()('Throw coins')
+          , div({class: 'flex flex-row'})([
+            , CoinSelection({
+              onChoice: (value) => dispatcher.chooseCoinSides(r, c, value)
+            }
+            )()
+            , CoinSelection({
+              onChoice: (value) => dispatcher.chooseCoinSides(r, c, value)
+            }
+            )()
+            , CoinSelection({
+              onChoice: (value) => dispatcher.chooseCoinSides(r, c, value)
+            }
+            )()
+          ])
+        ])
+        , div()([
+          , div()('Edit coins')
+          , div({ class: 'flex flex-row'})([
+            , AllCoinOptionsContent(model)
+            , LineNumbersContent(model)
+          ])
         ])
       ]
     )
